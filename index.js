c9e4cd81c1b4efb95deab203b579c6c4e7a69750
rafaelMunoz77 <rafael.munoz77@gmail.com>
2020-10-17 12:00:51 +0200

    Módulo 03 Vídeo 08 - Formulario autentica y pide mensajes

---
+++ index.js
@@ -0,0 +1,74 @@
+/**
+ * Función que se encarga de realizar un petición AJAX para autenticar usuario y obtener un JWT
+ */
+function autenticaUsuario() {
+    // Voy a enviar un JSON al seridor, contendrá los valores escritos por el usuario del formulario.
+    // Estos valores se encapsulan en un objeto JavaScript. Más adelante ese objeto se convertirá en 
+    // una cadena de caracteres (serialización) y se enviará junto con la petición web.
+    var jsonObject = {
+        usuario: document.getElementById("usuario").value,  // Utilizo el id de los campos del formulario
+        password: document.getElementById("password").value
+    }; 
+
+    // Sólo a efectos de depuración muestro el objeto que voy a enviar al servidor
+    console.log("u: " + jsonObject.usuario + " - p: " + jsonObject.password);
+
+    // Una petición XMLHttpRequest es parecido a un request, pero mucho más rico. Por ejemplo, permite usar
+    // formato JSON para encapsular la información que viaja entre cliente y servidor.
+    var xhttp = new XMLHttpRequest();
+
+    // Los objetos XMLHttpRequest permiten establecer una función de CallBack, esto es una función
+    // asíncrona que se ejecutará en diferentes momentos, en los que el estado de la respuesta esperada
+    // cambie. La respuesta puede pasar por varios "readyState":
+    //  - 1 - OPENED - La petición web se acaba de enviar.
+    //  - 2 - HEADERS_RECEIVED - Disponemos de las cabeceras y el estado de la respuesta del servidor
+    //  - 3 - LOADING - La respuesta completa del servidor se está descargando.
+    //  - 4 - DONE - La respuesta del servidor está completamente disponible.
+    // Además, una respuesta también tiene varios "status". Se puede consultar lista completa en https://www.restapitutorial.com/httpstatuscodes.html
+    // Baste decir que el éxito de la respuesta es el código 200.
+    xhttp.onreadystatechange = function () {
+        // Aunque dispongamos del estado de una respuesta, necesitamos esperar hasta que la carga
+        // de la misma ha terminado.
+        if (this.readyState == 4 && this.status == 200) {
+            // Cuando obtengo la respuesta, que es una cadena de texto en formato JSON, con un campo llamado "jwt"
+            // parseo ese texto para convertirlo en un objeto JavaScript
+            var objConJWT = JSON.parse(this.response);
+            // Utilizo el jwt para pedir un listado de mensajes de la bandeja de entrada del usuario encriptado en el JWT
+            pedirListadoMensajesMedianteJWT(objConJWT.jwt);
+        }
+    };
+    // Voy a configurar una petición a enviar al servidor. Se trata de autenticar al usuario
+    xhttp.open("POST", "http://localhost:8080/usuario/autentica", true);
+    // Especifico una cabecera en la petición (request) para indicar que la comunicación es JSON y UTF-8
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    // Con JSON.stringify serializo el objeto y lo convierto en formato JSON.
+    // Con la función "send(...)" anexo el texto JSON a la petición y la envío. Ahora toca esperar
+    // a que se ejecute la función de callback y los valores de readyState y status de la respuesta sean correctos
+    xhttp.send(JSON.stringify(jsonObject));
+}
+
+
+
+
+/*
+* Función que recibe un JWT válido, lo incorpora a la cabecera de una petición XHR y pide la lista
+* de mensajes de la bandeja de entrada del usuario cuyo id va encriptado dentro del JWT.
+* Obtiene únicamente los 10 primeros mensajes.
+*/
+function pedirListadoMensajesMedianteJWT (jwt) {
+    var xhttp = new XMLHttpRequest();  // Creación del objeto XHR
+
+    // Función de callback. Se ejecutará cada vez que la respuesta a la petición XHR cambie de estado
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) { // readyState 4 = "resp. recibida" y status = 200 "Todo Ok"
+            // Escribo la respuesta dentro de un div que tiene el html
+            document.getElementById("respuestaDelServidor").innerHTML = this.response; 
+        }
+    };
+    // Abro la petición XHR
+    xhttp.open("GET", "http://localhost:8080/mensajes/recibidos?pagina=0&mensajesPorPagina=10", true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8"); // Configuro la petición
+    xhttp.setRequestHeader("Authorization", "Bearer " + jwt); // Incluyo el jwt de autenticación
+    xhttp.send(); // Envío la petición XHR
+}
+
